221d2ee00a7edb92ced0f6d52535fb995f876724
VandelayUtd <drewhadley.guitar@gmail.com>
2022-11-30 16:42:29 -0500

    wrote dependencies for package.json

---
+++ server.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
+const { verify } = require('crypto');
+
+require('dotenv').config();
+
+const PORT = 3000;
