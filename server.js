b28cf8590c2cd2d592dacc14720b4aef3afea531
VandelayUtd <drewhadley.guitar@gmail.com>
2023-01-13 11:39:35 -0500

    added self signed certs

--- server.js
+++ server.js
@@ -11,3 +11,23 @@ const { verify } = require('crypto');
 require('dotenv').config();
 
 const PORT = 3000;
+
+const app = express();
+
+app.use(helmet());
+
+app.get('/secret', (req, res) => {
+    return res.send('your personal secrete value is 42')
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
+https.createServer({
+    key: fs.readFileSync('key.pem')
+})
+
+app.listen(PORT, ()=> {
+    console.log(`listening on port ${PORT}`)
+})
